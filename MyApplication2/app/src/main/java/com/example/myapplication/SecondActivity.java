8293485b90f9e4907fdd34e1599a9708b3961cb3
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2023-09-14 13:54:51 +0300

    First Lab
    This is the first lab with activity views

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void showGreetings(View view) {
+        String button_text;
+        button_text = ((Button) view).getText().toString();
+        if (button_text.equals("Open Main Activity")) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
